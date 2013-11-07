615ba09d1b8f99b7595cb3a8711677b03511277f
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2013-11-07 11:02:16 +0100

    added next() no server,program or tag is selected

--- routes/cms.js
+++ routes/cms.js
@@ -7,27 +7,36 @@ var db;
 
 var singleitem = function (req, res, next) {
 	var server = req.params.server;
+	
 	var program = req.params.program;
 	var tag = req.params.tag;
 	if (typeof server=='undefined'){server=1};
 	if (typeof program=='undefined'){program=1};
 	if (typeof tag=='undefined'){tag=0};
-	db.all("select id,name from servers order by name", function(err, rows) {
+	
+	if (isNaN(server)){
+		next();
+	}
+	if (isNaN(program)){
+		next();
+	}
+	if (isNaN(tag)){
+		next();
+	}
+	
+	db.all("select id,name from servers order by name", function(err, servers) {
 		if (err){
 			res.json({});
 		}
-		var servers = rows;
-		db.all("select id,name from programs join data on programs.id=data.program_id where server_id="+server+"  group by id,name", function(err, rows) {
+		db.all("select id,name from programs join data on programs.id=data.program_id where server_id="+server+"  group by id,name", function(err, programs) {
 			if (err){
 				res.json({});
 			}
-			var programs = rows;
-			db.all("select id,name from tags join data on tags.id=data.tag_id where server_id="+server+" and program_id="+program+"  group by id,name", function(err, rows) {
+			db.all("select id,name from tags join data on tags.id=data.tag_id where server_id="+server+" and program_id="+program+"  group by id,name", function(err, tags) {
 				if (err){
 					res.json({});
 				}
-				var tags = rows;
-				
+				 
 				res.render('index', {
 					title: 'title',
 					servers: servers,
