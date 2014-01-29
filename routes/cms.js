628a51c57ad99ea9838012ed405d7a6e825e6a9e
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2014-01-29 09:32:59 +0100

    filtering on time

--- routes/cms.js
+++ routes/cms.js
@@ -7,74 +7,74 @@ var db;
 var logger;
 
 var singleitem = function (req, res, next) {
-	var server = req.params.server;
-	
-	var program = req.params.program;
-	var tag = req.params.tag;
-	if (typeof server=='undefined'){server=1};
-	if (typeof program=='undefined'){program=1};
-	if (typeof tag=='undefined'){tag=0};
-	
-	if (isNaN(server)){
-		return next();
-		
-	}
-	if (isNaN(program)){
-		return next();
-	}
-	if (isNaN(tag)){
-		return next();
-	}
-	var timestamp = Math.round(new Date().getTime());
-	logger.log('debug','start ID '+timestamp);
-	db.all("select id,name from servers order by name", function(err, servers) {
-		logger.log('debug','ID '+timestamp+' servers: '+(Math.round(new Date().getTime())-timestamp ) );
-		if (err){
-			return res.json({err: err});
-		}
-		//select id,name from programs join data on programs.id=data.program_id where server_id=1  group by id,name
-		db.all("select id,name from programs join server_programs on programs.id=server_programs.program_id where server_id="+server+"  group by id,name", function(err, programs) {
-			logger.log('debug','ID '+timestamp+' programs: '+(Math.round(new Date().getTime())-timestamp ) );
-			if (err){
-				return res.json({err: err});
-			}
-			db.all("select id,name from tags join server_programs_tags on tags.id=server_programs_tags.tag_id where server_id="+server+" and program_id="+program+"  group by id,name", function(err, tags) {
-				logger.log('debug','ID '+timestamp+' tags: '+(Math.round(new Date().getTime())-timestamp ) );
-			
-				if (err){
-					return res.json({err: err});
-				}
-				
-				res.render('index', {
-					title: 'title',
-					servers: servers,
-					programs: programs,
-					tags: tags,
-					server: server,
-					program: program,
-					tag: tag
-				});
-				logger.log('debug','done '+timestamp+'' );
-			});
-		});
-	});
-	
-	
-	
+    var server = req.params.server;
+
+    var program = req.params.program;
+    var tag = req.params.tag;
+    if (typeof server=='undefined'){server=1};
+    if (typeof program=='undefined'){program=1};
+    if (typeof tag=='undefined'){tag=0};
+
+    if (isNaN(server)){
+        return next();
+
+    }
+    if (isNaN(program)){
+        return next();
+    }
+    if (isNaN(tag)){
+        return next();
+    }
+    var timestamp = Math.round(new Date().getTime());
+    logger.log('debug','start ID '+timestamp);
+    db.all("select id,name from servers order by name", function(err, servers) {
+        logger.log('debug','ID '+timestamp+' servers: '+(Math.round(new Date().getTime())-timestamp ) );
+        if (err){
+            return res.json({err: err});
+        }
+        //select id,name from programs join data on programs.id=data.program_id where server_id=1  group by id,name
+        db.all("select id,name from programs join server_programs on programs.id=server_programs.program_id where server_id="+server+"  group by id,name", function(err, programs) {
+            logger.log('debug','ID '+timestamp+' programs: '+(Math.round(new Date().getTime())-timestamp ) );
+            if (err){
+                return res.json({err: err});
+            }
+            db.all("select id,name from tags join server_programs_tags on tags.id=server_programs_tags.tag_id where server_id="+server+" and program_id="+program+"  group by id,name", function(err, tags) {
+                logger.log('debug','ID '+timestamp+' tags: '+(Math.round(new Date().getTime())-timestamp ) );
+
+                if (err){
+                    return res.json({err: err});
+                }
+
+                res.render('index', {
+                    title: 'title',
+                    servers: servers,
+                    programs: programs,
+                    tags: tags,
+                    server: server,
+                    program: program,
+                    tag: tag
+                });
+                logger.log('debug','done '+timestamp+'' );
+            });
+        });
+    });
+
+
+
 }
 
 
 
 var initRoute=function(app,options){
-	//access=ac;
-	//app.use(mainnavigation);
-	db = options.db;
-	logger = options.logger;
-	
-	app.get('/', singleitem);
-	app.get('/:server', singleitem);
-	app.get('/:server/:program', singleitem);
-	app.get('/:server/:program/:tag', singleitem);
+    //access=ac;
+    //app.use(mainnavigation);
+    db = options.db;
+    logger = options.logger;
+
+    app.get('/', singleitem);
+    app.get('/:server', singleitem);
+    app.get('/:server/:program', singleitem);
+    app.get('/:server/:program/:tag', singleitem);
 }
 
 exports.initRoute = initRoute;
--- routes/collect.js
+++ routes/collect.js
@@ -6,105 +6,111 @@ var tagsTable;
 
 
 var singleitem = function (req, res, next) {
-	var server = req.params.server;
-	var program = req.params.program;
-	var tag = req.params.tag;
-	
-	var show = ''; // default way
-	if (req.body){
-		if (req.body.show){
-			show = req.body.show;
-		}
-	}
-	
-	if (isNaN(server)){
-		return next();
-		
-	}
-	if (isNaN(program)){
-		return next();
-	}
-	if (isNaN(tag)){
-		return next();
-	}
-	var timestamp = Math.round(new Date().getTime());
-	logger.log('debug','start ID (collect) '+timestamp);
-	
-	
-	var tag_filter =" and tag_id="+tag+" ";
-	if (tag==0){
-		tag_filter = " ";
-		//sql = "SELECT server_id,program_id,tag_id,time,val FROM data where server_id="+server+" and program_id="+program+"  order by time desc limit 250";
-	}
-	var group_scale = 60 * 60 * 1000; // every hour
-	var limit = 300;
-	var sql = "SELECT server_id,program_id,tag_id,time t,val v FROM data where server_id="+server+" and program_id="+program+" "+tag_filter+" order by t desc limit "+limit;
-	switch(show){
-		case 'month':
-				limit = (5* 24 * 60 * 60 * 1000)/group_scale;
-				sql = "SELECT server_id,program_id,tag_id,floor(time/"+group_scale+")*"+group_scale+" t,avg(val) v FROM data where server_id="+server+" and program_id="+program+" "+tag_filter+" group by server_id,program_id,tag_id,t order by t desc limit "+limit;
-			break;
-		case 'week':
-				limit = (7* 24 * 60 * 60 * 1000)/group_scale;
-				sql = "SELECT server_id,program_id,tag_id,floor(time/"+group_scale+")*"+group_scale+" t,avg(val) v FROM data where server_id="+server+" and program_id="+program+" "+tag_filter+" group by server_id,program_id,tag_id,t order by t desc limit "+limit;
-			break;
-		case 'day':
-				limit = ( 24 * 60 * 60 * 1000)/group_scale;
-				sql = "SELECT server_id,program_id,tag_id,floor(time/"+group_scale+")*"+group_scale+" t,avg(val) v FROM data where server_id="+server+" and program_id="+program+" "+tag_filter+" group by server_id,program_id,tag_id,t order by t desc limit "+limit;
-			break;
-	}
-	//console.log(sql);
-	db.all(sql, function(err, rows) {
-		logger.log('debug','ID collect '+timestamp+' data: '+(Math.round(new Date().getTime())-timestamp ) );
-		if (err){
-			return res.json({});
-		}
-		var tagList = {};
-		var tagCount = 0;
-		rows.forEach(function (row) {
-			//console.log("data: "+JSON.stringify(row));
-			if (typeof tagList['T'+row.tag_id]=='undefined'){
-				tagList['T'+row.tag_id] = {
-					values: [],
-					tag: row.tag_id,
-					key: tagsTable.get(row.tag_id)/* ,
+    var server = req.params.server;
+    var program = req.params.program;
+    var tag = req.params.tag;
+
+    var show = ''; // default way
+    if (req.body){
+        if (req.body.show){
+            show = req.body.show;
+        }
+    }
+
+    if (isNaN(server)){
+        return next();
+
+    }
+    if (isNaN(program)){
+        return next();
+    }
+    if (isNaN(tag)){
+        return next();
+    }
+    var timestamp = Math.round(new Date().getTime());
+    logger.log('debug','start ID (collect) '+timestamp);
+
+
+    var tag_filter =" and tag_id="+tag+" ";
+    if (tag==0){
+        tag_filter = " ";
+        //sql = "SELECT server_id,program_id,tag_id,time,val FROM data where server_id="+server+" and program_id="+program+"  order by time desc limit 250";
+    }
+    
+    var mintime =(new Date()).getTime() - (1* 24 * 60 * 60 * 1000);
+    
+    var group_scale = 60 * 60 * 1000; // every hour
+    var limit = 300;
+    var sql = "SELECT server_id,program_id,tag_id,time t,val v FROM data where time >"+mintime+" and  server_id="+server+" and program_id="+program+" "+tag_filter+" order by t desc limit "+limit;
+    switch(show){
+        case 'month':
+            mintime = (new Date()).getTime() - (31* 24 * 60 * 60 * 1000)
+            limit = (5* 24 * 60 * 60 * 1000)/group_scale;
+            sql = "SELECT server_id,program_id,tag_id,floor(time/"+group_scale+")*"+group_scale+" t,avg(val) v FROM data where time >"+mintime+" and  server_id="+server+" and program_id="+program+" "+tag_filter+" group by server_id,program_id,tag_id,t order by t desc limit "+limit;
+            break;
+        case 'week':
+            mintime = (new Date()).getTime() - (7* 24 * 60 * 60 * 1000)
+            limit = (7* 24 * 60 * 60 * 1000)/group_scale;
+            sql = "SELECT server_id,program_id,tag_id,floor(time/"+group_scale+")*"+group_scale+" t,avg(val) v FROM data where time >"+mintime+" and  server_id="+server+" and program_id="+program+" "+tag_filter+" group by server_id,program_id,tag_id,t order by t desc limit "+limit;
+            break;
+        case 'day':
+            mintime = (new Date()).getTime() - (1* 24 * 60 * 60 * 1000)
+            limit = ( 24 * 60 * 60 * 1000)/group_scale;
+            sql = "SELECT server_id,program_id,tag_id,floor(time/"+group_scale+")*"+group_scale+" t,avg(val) v FROM data where time >"+mintime+" and  server_id="+server+" and program_id="+program+" "+tag_filter+" group by server_id,program_id,tag_id,t order by t desc limit "+limit;
+            break;
+    }
+    //console.log(sql);
+    db.all(sql, function(err, rows) {
+        logger.log('debug','ID collect '+timestamp+' data: '+(Math.round(new Date().getTime())-timestamp ) );
+        if (err){
+            return res.json({});
+        }
+        var tagList = {};
+        var tagCount = 0;
+        rows.forEach(function (row) {
+            //console.log("data: "+JSON.stringify(row));
+            if (typeof tagList['T'+row.tag_id]=='undefined'){
+                tagList['T'+row.tag_id] = {
+                    values: [],
+                    tag: row.tag_id,
+                    key: tagsTable.get(row.tag_id)/* ,
 					color: (colorList[tagCount])?colorList[tagCount]:colorList[0]*/
-				};
-				tagCount++;
-			}
-			tagList['T'+row.tag_id].values.push({
-				x: row.t,
-				y: row.v
-			});
-		});
-		var output=[];
-		for(var t in tagList){
-			var list=tagList[t].values;
-			list.reverse();
-			var item = {
-				values: list,
-				tag: tagList[t].tag,
-				key: tagList[t].key/*,
+                };
+                tagCount++;
+            }
+            tagList['T'+row.tag_id].values.push({
+                x: row.t,
+                y: row.v
+            });
+        });
+        var output=[];
+        for(var t in tagList){
+            var list=tagList[t].values;
+            list.reverse();
+            var item = {
+                values: list,
+                tag: tagList[t].tag,
+                key: tagList[t].key/*,
 				color: tagList[t].color*/
-			}
-			output.push(item);
-		}
-		res.json({
-			list:output,
-			preveredChart: 'lineChart'
-		});
-		logger.log('debug','ID collect '+timestamp+' done');
-	});
+            }
+            output.push(item);
+        }
+        res.json({
+            list:output,
+            preveredChart: 'lineChart'
+        });
+        logger.log('debug','ID collect '+timestamp+' done');
+    });
 }
 
 var initRoute=function(app,options){
-	db= options.db;
-	logger=options.logger;
-	serversTable=options.serversTable;
-	programsTable=options.programsTable;
-	tagsTable=options.tagsTable;
-	app.get('/chartData/:server/:program/:tag', singleitem);
-	app.post('/chartData/:server/:program/:tag', singleitem);
+    db= options.db;
+    logger=options.logger;
+    serversTable=options.serversTable;
+    programsTable=options.programsTable;
+    tagsTable=options.tagsTable;
+    app.get('/chartData/:server/:program/:tag', singleitem);
+    app.post('/chartData/:server/:program/:tag', singleitem);
 }
 
 exports.initRoute = initRoute;
